2104355a0839d88892f04653dac7df31c86eb801
Rafał Sojecki <s26286@pjwstk.edu.pl>
2022-03-19 16:07:32 +0100

    rozwiazanie

---
+++ kolokwium2/main.java
@@ -0,0 +1,21 @@
+public class main {
+	public static String sum(String[] array) {
+		int returned = 0;
+		for (int i = 0; i < array.length; i++) {
+			try {
+				int a = Integer.parseInt(array[i]);
+				returned += a;
+				System.out.println(a);
+			} catch (Exception e) {
+				
+			}
+			
+		}
+		return returned + "";
+	}
+
+	public static void main(String[] args) {
+		String[] tablica = { "1", "2", "d", "3"};
+		System.out.println(sum(tablica));
+	}
+}
